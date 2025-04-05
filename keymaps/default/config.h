4688b55d315fef9d3c79a83c0caafcc6f09d6e89
Tejas <tejas.shetty@mailbox.org>
2025-04-05 23:29:14 +0530

    Geist/Totem Custom Minimal Config

---
+++ keymaps/default/config.h
@@ -0,0 +1,22 @@
+// Copyright 2025 @shetty-tejas
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+#define DYNAMIC_MACRO_NO_NESTING
+
+#define MASTER_LEFT // TODO: Change this to EE_HANDS once I get TRRS (after all testing ofc)
+
+#define NO_ACTION_ONESHOT
+
+#define OS_DETECTION_KEYBOARD_RESET
+#define OS_DETECTION_SINGLE_REPORT
+
+#define TAPPING_TERM 225
+#define QUICK_TAP_TERM TAPPING_TERM
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
